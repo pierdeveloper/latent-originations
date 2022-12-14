21a5b6cdf27be00fe7c25b8ca71b61b0bec6b331
Alex <ahegevall@gmail.com>
2022-12-14 09:28:58 -0800

    12-14-22 attempting to begin error refactor

--- routes/api/coverage.js
+++ routes/api/coverage.js
@@ -7,10 +7,13 @@ const config = require('config');
 // @desc      Retrieve list of commercial credit coverage by state
 // @access    Public
 router.get('/commercial', [auth], async (req, res) => {
-    const states = config.get('states')
-    console.log(states)
-    res.json(states)
-    
+    try {
+        const states = config.get('states')
+        res.json(states)
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 })
 
 
